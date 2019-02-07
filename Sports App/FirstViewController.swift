9f7386aa157d0aaa78cb9dc03aa9e90afcc8e0f1
daytonflores <dayton.flores@yahoo.com>
2019-02-07 14:27:27 -0800

    Initial Commit

---
+++ Sports App/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  Sports App
+//
+//  Created by Dayton Flores on 2/7/19.
+//  Copyright © 2019 CS 472. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
