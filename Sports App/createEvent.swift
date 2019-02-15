ff1073128451b44b652f91d70ce1ab3355e14c9d
Joseph <jsharphalpin@josephs-imac.local>
2019-02-15 12:52:34 -0800

    Added Mapview in CreateEvent Page

--- Sports App/createEvent.swift
+++ Sports App/createEvent.swift
@@ -7,9 +7,12 @@
 //
 
 import UIKit
+import MapKit
 
-class createEvent: UIViewController {
-
+class createEvent: UIViewController, UISearchBarDelegate {
+    
+    @IBOutlet weak var _myMapView: MKMapView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -26,5 +29,69 @@ class createEvent: UIViewController {
         // Pass the selected object to the new view controller.
     }
     */
-
+    
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        //Ignore user
+        UIApplication.shared.beginIgnoringInteractionEvents()
+        
+        //Activity Indicator
+        let activityIndicator = UIActivityIndicatorView()
+        activityIndicator.style = UIActivityIndicatorView.Style.gray
+        activityIndicator.center = self.view.center
+        activityIndicator.hidesWhenStopped = true
+        activityIndicator.startAnimating()
+        
+        self.view.addSubview(activityIndicator)
+        
+        //Hide search bar
+        searchBar.resignFirstResponder()
+        dismiss(animated: true, completion: nil)
+        
+        //create the search request
+        let searchRequest = MKLocalSearch.Request()
+        searchRequest.naturalLanguageQuery = searchBar.text
+        
+        let activeSearch = MKLocalSearch(request: searchRequest)
+        
+        activeSearch.start { (response, error) in
+            
+            activityIndicator.stopAnimating()
+            UIApplication.shared.endIgnoringInteractionEvents()
+            
+            if response == nil{
+                print("ERROR")
+            }
+            else{
+                //remove annotations
+                let annotations = self._myMapView.annotations
+                self._myMapView.removeAnnotations(annotations)
+                
+                //getting data
+                let latitude = response?.boundingRegion.center.latitude
+                let longitude = response?.boundingRegion.center.longitude
+                
+                //create annotation
+                let annotation = MKPointAnnotation()
+                annotation.title = searchBar.text
+                annotation.coordinate = CLLocationCoordinate2DMake(latitude!, longitude!)
+                self._myMapView.addAnnotation(annotation)
+                
+                //zoom in on annotation
+                let coordinate:CLLocationCoordinate2D = CLLocationCoordinate2DMake(latitude!, longitude!)
+                let span = MKCoordinateSpan(latitudeDelta: 0.1, longitudeDelta: 0.1)
+                let region = MKCoordinateRegion(center: coordinate, span: span)
+                self._myMapView.setRegion(region, animated: true)
+                
+            }
+            
+        }
+        
+    }
+    
+    @IBAction func searchButton(_ sender: Any) {
+        let searchController = UISearchController(searchResultsController: nil)
+        searchController.searchBar.delegate = self
+        present(searchController, animated: true, completion: nil)
+    }
+    
 }
