712c2b99f262601b9d7639ba1ffbdf6d149d4379
flored1 <flored1@unlv.nevada.edu>
2019-03-04 12:55:01 -0800

    - reverted to last working version of app

--- Sports App/homeTab.swift
+++ Sports App/homeTab.swift
@@ -8,67 +8,41 @@
 
 import UIKit
 
-class homeTab: UIViewController, UITextFieldDelegate, UIPickerViewDelegate, UIPickerViewDataSource {
+class homeTab: UIViewController/*, UITextFieldDelegate, UIPickerViewDelegate, UIPickerViewDataSource*/ {
 
-    
+/*
     //variable for sport label
     @IBOutlet weak var Label: UILabel!
     
     //variable for picker view for sports
     @IBOutlet weak var Sports: UIPickerView!
-    
-    /*
-    //list for sports picker view
-    let Sports_List = ["Baseball", "Basketball", "Football", "Hockey", "Soccer", "Tennis", "Volleyball"]
-    
-    //list for radius picker view
-    let Radius_List = ["1 mile", "2 miles", "3 miles", "4 miles", "5 miles", "10 miles", "15 miles", "20 miles", "30 miles"]
-    
-    //column amount function for picker view
-    func numberOfComponents(in pickerView: UIPickerView) -> Int {
-        return 1   //only one column
-    }
-    
-    //name for each row function in picker view
-    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
-        return Sports_List[row]
-    }
-    
-    //row amount function for picker view
-    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
-        return Sports_List.count
-    }
-    
-    //places selected sport in label
-    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
-        Label.text = Sports_List[row]
-    } */
-    
+
     
     var currentTextField = UITextField()
     var pickerView = UIPickerView()
     
     var SportsList:[String] = []
     var RadiusList:[String] = []
-    
+ 
     
     
     @IBOutlet weak var Sports_Field: UITextField!
 
     @IBOutlet weak var Radius_Field: UITextField!
     
-    
+*/
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
         
-        SportsList = ["Baseball", "Basketball", "Football", "Hockey", "Soccer", "Tennis", "Volleyball"]
-        RadiusList = ["1 mile", "2 miles", "3 miles", "4 miles", "5 miles", "10 miles", "15 miles", "20 miles", "30 miles"]
+//        SportsList = ["Baseball", "Basketball", "Football", "Hockey", "Soccer", "Tennis", "Volleyball"]
+//        RadiusList = ["1 mile", "2 miles", "3 miles", "4 miles", "5 miles", "10 miles", "15 miles", "20 miles", "30 miles"]
         
   }
 
+/*
     func numberOfComponents(in pickerView: UIPickerView) -> Int {
         return 1   //only one column
     }
@@ -121,7 +95,7 @@ class homeTab: UIViewController, UITextFieldDelegate, UIPickerViewDelegate, UIPi
         }
     }
 
-
+*/
     
     
     /*
