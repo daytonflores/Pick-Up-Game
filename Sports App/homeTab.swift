e981bb284cae686353ab348d0e43dab5c8ede840
Frank Frisbee <frisbee@unlv.nevada.edu>
2019-04-01 18:55:19 -0700

    Set up table view in feed view for viewing events

--- Sports App/homeTab.swift
+++ Sports App/homeTab.swift
@@ -7,78 +7,41 @@
 //
 
 import UIKit
+import FirebaseAuth
+import FirebaseDatabase
 
-class homeTab: UIViewController, UITextFieldDelegate, UIPickerViewDelegate, UIPickerViewDataSource {
+class homeTab: UIViewController {
 
-
-    @IBOutlet weak var Sport: UILabel!
-    @IBOutlet weak var Radius: UILabel!
-    
-    
-    @IBOutlet weak var SportsPicker: UIPickerView!
-    @IBOutlet weak var RadiusPicker: UIPickerView!
-    
-    
-    var SportsList:[String] = []
-    var RadiusList:[String] = []
-    
-    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-        
-        SportsList = ["Baseball", "Basketball", "Football", "Hockey", "Soccer", "Tennis", "Volleyball"]
-        RadiusList = ["1 mile", "2 miles", "3 miles", "4 miles", "5 miles", "10 miles", "15 miles", "20 miles", "30 miles"]
-        
+        tableView.dataSource = self
+        loadEvents()
     }
     
-    func numberOfComponents(in pickerView: UIPickerView) -> Int {
-        return 1
-    }
-
-    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
-        if (pickerView.tag == 1) {
-            return SportsList.count
-        }
-        else {
-            return RadiusList.count
-        }
-    }
-
-    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
-        if (pickerView.tag == 1) {
-            return SportsList[row]
-        }
-        else {
-            return RadiusList[row]
-        }
-    }
-
-    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
-        if (pickerView.tag == 1) {
-            self.Sport.text = self.SportsList[row]
-        }
-        else {
-            self.Radius.text = self.RadiusList[row]
+    func loadEvents() {
+        Database.database().reference().child("event").observe(.value) { (snapshot: DataSnapshot) in
+            print(snapshot.value as Any)
         }
     }
     
+    @IBOutlet weak var tableView: UITableView!
     
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    } */
- 
-    
-    @IBAction func homeTabUnwind(segue: UIStoryboardSegue)
-    {
-    }
- 
 }
 
+    
+func homeTabUnwind(segue: UIStoryboardSegue)
+    {
+    }
 
+extension homeTab: UITableViewDataSource {
+    func tableView (_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt
+        indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text = "\(indexPath.row)"
+        cell.backgroundColor = UIColor.blue
+        return cell
+    }
+}
