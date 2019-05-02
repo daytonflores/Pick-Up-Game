efac4bf0411a4058e2e328d1a536257e020c75d1
Erik Chow <bunzofplastic@gmail.com>
2019-05-02 15:16:15 -0700

    Replaced home feed profile pics with sport icons

--- Sports App/EventTableViewCell.swift
+++ Sports App/EventTableViewCell.swift
@@ -45,43 +45,62 @@ class EventTableViewCell: UITableViewCell {
         
         photoext = String(post.creator + ".jpeg")
         
-        readRef.child("photo").observe(.value){
-            (snapshot) in
-            self.photourl = snapshot.value as? String
-            if(self.photourl == "https://firebasestorage.googleapis.com/v0/b/tryone-de29a.appspot.com/o/Anonymous.jpg?alt=media&token=4ed6f927-cfc7-4693-91dc-8774c36ce257"){
-                
-                let picRef = self.storageRef.child("Anonymous.jpg")
-                
-                // Download in memory with a maximum allowed size of 1MB (1 * 1024 * 1024 bytes)
-                picRef.getData(maxSize: 1 * 1024 * 1024 * 5) { data, error in
-                    if error != nil {
-                        // Uh-oh, an error occurred!
-                    } else {
-                        // Data for "images/island.jpg" is returned
-                        let image = UIImage(data: data!)
-                        self._ProfilePic.image = image
-                    }
-                }
-                
-            }
-            else{
-                // Create a reference to the file you want to download
-                let picRef = self.storageRef.child(self.photoext!)
-                
-                // Download in memory with a maximum allowed size of 1MB (1 * 1024 * 1024 bytes)
-                picRef.getData(maxSize: 1 * 1024 * 1024 * 5) { data, error in
-                    if error != nil {
-                        // Uh-oh, an error occurred!
-                    } else {
-                        // Data for "images/island.jpg" is returned
-                        let image = UIImage(data: data!)
-                        self._ProfilePic.image = image
-                    }
-                }
-            }
+//        readRef.child("photo").observe(.value){
+//            (snapshot) in
+//            self.photourl = snapshot.value as? String
+//            if(self.photourl == "https://firebasestorage.googleapis.com/v0/b/tryone-de29a.appspot.com/o/Anonymous.jpg?alt=media&token=4ed6f927-cfc7-4693-91dc-8774c36ce257"){
+//
+//                let picRef = self.storageRef.child("Anonymous.jpg")
+//
+//                // Download in memory with a maximum allowed size of 1MB (1 * 1024 * 1024 bytes)
+//                picRef.getData(maxSize: 1 * 1024 * 1024 * 5) { data, error in
+//                    if error != nil {
+//                        // Uh-oh, an error occurred!
+//                    } else {
+//                        // Data for "images/island.jpg" is returned
+//                        let image = UIImage(data: data!)
+//                        self._ProfilePic.image = image
+//                    }
+//                }
+//
+//            }
+//            else{
+//                // Create a reference to the file you want to download
+//                let picRef = self.storageRef.child(self.photoext!)
+//
+//                // Download in memory with a maximum allowed size of 1MB (1 * 1024 * 1024 bytes)
+//                picRef.getData(maxSize: 1 * 1024 * 1024 * 5) { data, error in
+//                    if error != nil {
+//                        // Uh-oh, an error occurred!
+//                    } else {
+//                        // Data for "images/island.jpg" is returned
+//                        let image = UIImage(data: data!)
+//                        self._ProfilePic.image = image
+//                    }
+//                }
+//            }
+//
+//            // Load the image using SDWebImage
+//
+//        }
+        switch post.sport {
+        case "Baseball":
+            _ProfilePic.image = UIImage(named: "icon_baseball.png")
+        case "Basketball":
+            _ProfilePic.image = UIImage(named: "icon_basketball.png")
+        case "Soccer":
+            _ProfilePic.image = UIImage(named: "icon_soccer.png")
+        case "Football":
+            _ProfilePic.image = UIImage(named: "icon_football.png")
+        case "Tennis":
+            _ProfilePic.image = UIImage(named: "icon_tennis.png")
+        case "Hockey":
+            _ProfilePic.image = UIImage(named: "icon_hockey.png")
+        case "Volleyball":
+            _ProfilePic.image = UIImage(named: "icon_volleyball.png")
             
-            // Load the image using SDWebImage
-            
+        default:
+            _ProfilePic.image = UIImage(named: "icon_baseball.png")
         }
         
         ref.child("users").child(post.creator).observe(DataEventType.value, with: { (snapshot) in        // updates if database entry changes
@@ -94,46 +113,46 @@ class EventTableViewCell: UITableViewCell {
             self.user?.text = self.username! + " wants to play"
             self._Sport?.text = post.sport
             if post.sport == "Basketball"{
-                self.location?.backgroundColor = UIColor(red: 1.0, green: 0.5, blue: 0.0, alpha: 0.2)
-                self.time?.backgroundColor = UIColor(red: 1.0, green: 0.5, blue: 0.0, alpha: 0.2)
-                self.user?.backgroundColor = UIColor(red: 1.0, green: 0.5, blue: 0.0, alpha: 0.2)
-                self._Sport?.backgroundColor = UIColor(red: 1.0, green: 0.5, blue: 0.0, alpha: 0.2)
+                self.location?.backgroundColor = UIColor(red: 1.0, green: 0.5, blue: 0.0, alpha: 0.1)
+                self.time?.backgroundColor = UIColor(red: 1.0, green: 0.5, blue: 0.0, alpha: 0.1)
+                self.user?.backgroundColor = UIColor(red: 1.0, green: 0.5, blue: 0.0, alpha: 0.1)
+                self._Sport?.backgroundColor = UIColor(red: 1.0, green: 0.5, blue: 0.0, alpha: 0.1)
             }
             else if post.sport == "Football"{
-                self.location?.backgroundColor = UIColor(red: 0.6, green: 0.4, blue: 0.2, alpha: 0.2)
-                self.time?.backgroundColor = UIColor(red: 0.6, green: 0.4, blue: 0.2, alpha: 0.2)
-                self.user?.backgroundColor = UIColor(red: 0.6, green: 0.4, blue: 0.2, alpha: 0.2)
-                self._Sport?.backgroundColor = UIColor(red: 0.6, green: 0.4, blue: 0.2, alpha: 0.2)
+                self.location?.backgroundColor = UIColor(red: 0.6, green: 0.4, blue: 0.1, alpha: 0.1)
+                self.time?.backgroundColor = UIColor(red: 0.6, green: 0.4, blue: 0.1, alpha: 0.1)
+                self.user?.backgroundColor = UIColor(red: 0.6, green: 0.4, blue: 0.1, alpha: 0.1)
+                self._Sport?.backgroundColor = UIColor(red: 0.6, green: 0.4, blue: 0.1, alpha: 0.1)
             }
             else if post.sport == "Tennis"{
-                self.location?.backgroundColor = UIColor(red: 1.0, green: 1.0, blue: 0.0, alpha: 0.2)
-                self.time?.backgroundColor = UIColor(red: 1.0, green: 1.0, blue: 0.0, alpha: 0.2)
-                self.user?.backgroundColor = UIColor(red: 1.0, green: 1.0, blue: 0.0, alpha: 0.2)
-                self._Sport?.backgroundColor = UIColor(red: 1.0, green: 1.0, blue: 0.0, alpha: 0.2)
+                self.location?.backgroundColor = UIColor(red: 1.0, green: 1.0, blue: 0.0, alpha: 0.1)
+                self.time?.backgroundColor = UIColor(red: 1.0, green: 1.0, blue: 0.0, alpha: 0.1)
+                self.user?.backgroundColor = UIColor(red: 1.0, green: 1.0, blue: 0.0, alpha: 0.1)
+                self._Sport?.backgroundColor = UIColor(red: 1.0, green: 1.0, blue: 0.0, alpha: 0.1)
             }
             else if post.sport == "Soccer"{
-                self.location?.backgroundColor = UIColor(red: 0.0, green: 1.0, blue: 0.0, alpha: 0.2)
-                self.time?.backgroundColor = UIColor(red: 0.0, green: 1.0, blue: 0.0, alpha: 0.2)
-                self.user?.backgroundColor = UIColor(red: 0.0, green: 1.0, blue: 0.0, alpha: 0.2)
-                self._Sport?.backgroundColor = UIColor(red: 0.0, green: 1.0, blue: 0.0, alpha: 0.2)
+                self.location?.backgroundColor = UIColor(red: 0.0, green: 1.0, blue: 0.0, alpha: 0.1)
+                self.time?.backgroundColor = UIColor(red: 0.0, green: 1.0, blue: 0.0, alpha: 0.1)
+                self.user?.backgroundColor = UIColor(red: 0.0, green: 1.0, blue: 0.0, alpha: 0.1)
+                self._Sport?.backgroundColor = UIColor(red: 0.0, green: 1.0, blue: 0.0, alpha: 0.1)
             }
             else if post.sport == "Volleyball"{
-                self.location?.backgroundColor = UIColor(red: 8.0, green: 0.0, blue: 8.0, alpha: 0.2)
-                self.time?.backgroundColor = UIColor(red: 8.0, green: 0.0, blue: 8.0, alpha: 0.2)
-                self.user?.backgroundColor = UIColor(red: 8.0, green: 0.0, blue: 8.0, alpha: 0.2)
-                self._Sport?.backgroundColor = UIColor(red: 8.0, green: 0.0, blue: 8.0, alpha: 0.2)
+                self.location?.backgroundColor = UIColor(red: 8.0, green: 0.0, blue: 8.0, alpha: 0.1)
+                self.time?.backgroundColor = UIColor(red: 8.0, green: 0.0, blue: 8.0, alpha: 0.1)
+                self.user?.backgroundColor = UIColor(red: 8.0, green: 0.0, blue: 8.0, alpha: 0.1)
+                self._Sport?.backgroundColor = UIColor(red: 8.0, green: 0.0, blue: 8.0, alpha: 0.1)
             }
             else if post.sport == "Hockey"{
-                self.location?.backgroundColor = UIColor(red: 1.0, green: 0.0, blue: 0.0, alpha: 0.2)
-                self.time?.backgroundColor = UIColor(red: 1.0, green: 0.0, blue: 0.0, alpha: 0.2)
-                self.user?.backgroundColor = UIColor(red: 1.0, green: 0.0, blue: 0.0, alpha: 0.2)
-                self._Sport?.backgroundColor = UIColor(red: 1.0, green: 0.0, blue: 0.0, alpha: 0.2)
+                self.location?.backgroundColor = UIColor(red: 1.0, green: 0.0, blue: 0.0, alpha: 0.1)
+                self.time?.backgroundColor = UIColor(red: 1.0, green: 0.0, blue: 0.0, alpha: 0.1)
+                self.user?.backgroundColor = UIColor(red: 1.0, green: 0.0, blue: 0.0, alpha: 0.1)
+                self._Sport?.backgroundColor = UIColor(red: 1.0, green: 0.0, blue: 0.0, alpha: 0.1)
             }
             else if post.sport == "Baseball"{
-                self.location?.backgroundColor = UIColor(red: 0.0, green: 0.5, blue: 1.0, alpha: 0.2)
-                self.time?.backgroundColor = UIColor(red: 0.0, green: 0.5, blue: 1.0, alpha: 0.2)
-                self.user?.backgroundColor = UIColor(red: 0.0, green: 0.5, blue: 1.0, alpha: 0.2)
-                self._Sport?.backgroundColor = UIColor(red: 0.0, green: 0.5, blue: 1.0, alpha: 0.2)
+                self.location?.backgroundColor = UIColor(red: 0.0, green: 0.5, blue: 1.0, alpha: 0.1)
+                self.time?.backgroundColor = UIColor(red: 0.0, green: 0.5, blue: 1.0, alpha: 0.1)
+                self.user?.backgroundColor = UIColor(red: 0.0, green: 0.5, blue: 1.0, alpha: 0.1)
+                self._Sport?.backgroundColor = UIColor(red: 0.0, green: 0.5, blue: 1.0, alpha: 0.1)
             }
             else{
                 self.location?.backgroundColor = UIColor.white
--- Sports App/homeTab.swift
+++ Sports App/homeTab.swift
@@ -133,25 +133,25 @@ extension homeTab: UITableViewDataSource {
         let sportcolor = eventCell.sport
         
         if sportcolor == "Basketball"{
-            cell.backgroundColor = UIColor(red: 1.0, green: 0.5, blue: 0.0, alpha: 0.3)
+            cell.backgroundColor = UIColor(red: 1.0, green: 0.5, blue: 0.0, alpha: 0.1)
         }
         else if sportcolor == "Football"{
-            cell.backgroundColor = UIColor(red: 0.6, green: 0.4, blue: 0.2, alpha: 0.3)
+            cell.backgroundColor = UIColor(red: 0.6, green: 0.4, blue: 0.2, alpha: 0.1)
         }
         else if sportcolor == "Tennis"{
-            cell.backgroundColor = UIColor(red: 1.0, green: 1.0, blue: 0.0, alpha: 0.3)
+            cell.backgroundColor = UIColor(red: 1.0, green: 1.0, blue: 0.0, alpha: 0.1)
         }
         else if sportcolor == "Soccer"{
-            cell.backgroundColor = UIColor(red: 0.0, green: 1.0, blue: 0.0, alpha: 0.3)
+            cell.backgroundColor = UIColor(red: 0.0, green: 1.0, blue: 0.0, alpha: 0.1)
         }
         else if sportcolor == "Volleyball"{
-            cell.backgroundColor = UIColor(red: 8.0, green: 0.0, blue: 8.0, alpha: 0.3)
+            cell.backgroundColor = UIColor(red: 8.0, green: 0.0, blue: 8.0, alpha: 0.1)
         }
         else if sportcolor == "Hockey"{
-            cell.backgroundColor = UIColor(red: 1.0, green: 0.0, blue: 0.0, alpha: 0.3)
+            cell.backgroundColor = UIColor(red: 1.0, green: 0.0, blue: 0.0, alpha: 0.1)
         }
         else if sportcolor == "Baseball"{
-            cell.backgroundColor = UIColor(red: 0.0, green: 0.5, blue: 1.0, alpha: 0.3)
+            cell.backgroundColor = UIColor(red: 0.0, green: 0.5, blue: 1.0, alpha: 0.1)
         }
         else{
             cell.backgroundColor = UIColor.white
